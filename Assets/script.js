f4b5a533478b8a864ed0d5e02443c6d084618507
amymckellar <amy_mckellar@yahoo.com>
2022-11-16 23:09:32 -0600

    creating variable in script.js

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+// adding variable for each question, getting the score, and making sure the question is correct
+var questions = {};
+var score = 0;
+var acceptingAnswers = true;
+var questionCounter = 0;
+
+// getting each specific variable and adding the questions and answers
